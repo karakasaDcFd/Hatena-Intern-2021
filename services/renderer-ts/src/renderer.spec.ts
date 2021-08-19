ef56dcaa488425167d8a69d80e38f41d8af3e010
KarakasaDcFd <recieveout1224@gmail.com>
2021-08-19 12:06:15 +0900

    Add tests for rendering Markdown syntax

--- services/renderer-ts/src/renderer.spec.ts
+++ services/renderer-ts/src/renderer.spec.ts
@@ -1,9 +1,35 @@
 import { render } from "./renderer";
 
 describe("render", () => {
-  it("URL の自動リンクができる", async () => {
-    const src = "foo https://google.com/ bar";
+  it("Markdownの見出し記法が使える", async () => {
+    const src = "## Title";
     const html = await render(src);
-    expect(html).toBe('foo <a href="https://google.com/">https://google.com/</a> bar');
+    expect(html).toBe('<h2>Title</h2>');
+  });
+});
+
+const listAns = `<ul>
+<li>List1
+<ul>
+<li>SubList1</li>
+<li>SubList2</li>
+</ul>
+</li>
+<li>List2</li>
+</ul>`
+
+describe("render", () => {
+  it("Markdownのリスト記法が使える", async () => {
+    const src = "- List1\n  - SubList1\n  - SubList2\n- List2";
+    const html = await render(src);
+    expect(html).toBe(listAns);
+  });
+});
+
+describe("render", () => {
+  it("Markdownのリンク記法が使える", async () => {
+    const src = "[テスト](https://example.com)";
+    const html = await render(src);
+    expect(html).toBe('<p><a href="https://example.com">テスト</a></p>');
   });
 });
